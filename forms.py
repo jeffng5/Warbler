b61c394ac9c3ae79f8438bf814f74e48b9f9cec8
Jeffrey Ng <jeffreyng@Jeffreys-Mini.fios-router.home>
2023-11-23 01:34:10 -0500

    first commit 65% completed

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    bio = TextAreaField('Bio')
+    location = StringField('Location')
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+# class ProfileEditForm(FlaskForm):
+
